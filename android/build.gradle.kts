e189bc4af4ffe2e0f62a2f13ecf50596a6dcbc73
dionisdel <dionisdel@gmail.com>
2026-09-04 19:25:47 -0400

    fix versiones y compatibilidad

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,11 +5,8 @@ allprojects {
     }
 }
 
-rootProject.layout.buildDirectory.value(rootProject.layout.projectDirectory.dir("../build"))
-
 subprojects {
     project.evaluationDependsOn(":app")
-    project.layout.buildDirectory.value(rootProject.layout.buildDirectory.dir(project.name))
 }
 
 tasks.register<Delete>("clean") {
